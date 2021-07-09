9caa9c75bc66e443da7e4a858c7e9ccbafd1f171
13tinydots <13tinydots@gmail.com>
2021-07-09 14:55:56 -0500

    updates to all files including inquirer logic and class refinements

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+const { SSL_OP_TLS_BLOCK_PADDING_BUG } = require('constants');
 const fs = require('fs');
+const inquirer = require("inquirer");
+const { inherits } = require('util');
+const {Employee} = require('./lib/classes');
+const {Manager}= require('./lib/classes');
+const {Engineer} = require('./lib/classes');
+const {Intern} = require('./lib/classes');
 
-const Employee = require('./lib/classes');
-const Manager = require('./lib/classes');
-// const Engineer = require('./lib/classes');
-// const Intern = require('./lib/classes');
+const arrOfEmployees = [];
 
-inquirer
-.prompt([
+const questions = ([
     {
         type:'list',
         name: 'role',
@@ -30,7 +33,83 @@ inquirer
     }
 ])
 
-.then((answers) =>{
-    answers:
-    
-})
+function askManagerQuestions({employeeName, employeeID, emailAddress, role}) {
+    inquirer.prompt([
+        {
+            message: "Office number?",
+            name: "officeNum",
+            type: "input"
+        }
+    ]).then(({officeNum}) => {
+        const manager = new Manager(employeeName, employeeID, emailAddress, role, officeNum)
+        arrOfEmployees.push(manager)
+        addAnotherEmployee();
+    })
+}
+
+function askEngineerQuestions({employeeName, employeeID, emailAddress, role}) {
+    inquirer.prompt([
+        {
+            message: "Github?",
+            name: "github",
+            type: "input"
+        }
+    ]).then(({github}) => {
+        const engineer = new Engineer(employeeName, employeeID, emailAddress, role, github)
+        arrOfEmployees.push(engineer)
+        addAnotherEmployee();
+    })
+}
+
+function askInternQuestions({employeeName, employeeID, emailAddress, role}) {
+    inquirer.prompt([
+        {
+            message: "School?",
+            name: "school",
+            type: "input"
+        }
+    ]).then(({school}) => {
+        const intern = new Intern(employeeName, employeeID, emailAddress, role, school)
+        arrOfEmployees.push(intern)
+        addAnotherEmployee();
+    })
+}
+// function writeToFile(fileName, data) {
+//     fs.writeFile(fileName, data, err=> {
+//         if(err) throw err
+//     } )
+// }
+
+function addAnotherEmployee(){
+    inquirer.prompt([
+        {
+            message: "Add another employee?",
+            name: "add",
+            type: "list",
+            choices: ["yes", "no"]
+        }
+    ]).then((response) => {
+        if (response === "yes") {
+            init();
+        } 
+        //else //insert HTML generate ref here.
+    })
+}
+
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((answers) => {
+        if(answers.role == "Manager"){
+            askManagerQuestions(answers)
+        } else if (answers.role == "Engineer"){
+            askEngineerQuestions(answers)
+        } else {
+            askInternQuestions(answers)
+        }
+    })
+}
+
+init()
+// const manager =  new Manager("Name", "Id", "Email", "Manager", "OfficeNum")
+// console.log(manager)
--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,8 +1,8 @@
-const Employee = require('../lib/classes');
+const {Employee} = require('../lib/classes');
 
 describe('Initialization', () =>{
     it('should create a new instance of an Employee', () => {
-        const employee = new Employee(bobby, "12345", "nomail@nomail.com");
+        const employee = new Employee("bobby", "12345", "nomail@nomail.com");
         expect(employee).toBeInstanceOf(Employee);
     })
 })
