ec08704be367c74a7f443aa3818be62e1c68016d
13tinydots <13tinydots@gmail.com>
2021-07-09 14:56:42 -0500

    class and template update

---
+++ src/template.js
@@ -0,0 +1,12 @@
+
+function generateHtml(data) {
+    for(i=0; i<data.length; i++){
+        if(data[i].getRole() == "Manager"{
+            templateLit  += `
+            some HTML about manager
+            `
+        })
+    }
+}
+
+module.exports = generateHtml;
