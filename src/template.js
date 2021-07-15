3ca1900f2f76ab8b904ad6fc7c9860c776a07913
13tinydots <13tinydots@gmail.com>
2021-07-14 19:55:44 -0500

    Expanded upon template file to include bulma styling and added a prototype page to work with the original html.

--- src/template.js
+++ src/template.js
@@ -1,13 +1,36 @@
 
 function generateHtml(data) {
     for(i=0; i<data.length; i++){
-        if(data[i].getRole() == "Manager") {
-            return `<h1>some HTML about manager</h1>
+        if(data[i].getRole() === "Manager") {
+            return `<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+                <title>My Team</title>
+            </head>
+                <section class="hero is-danger">
+                    <div class="hero-body">
+                        <h1 class="title is-2">
+                            My Team
+                        </h1>
+                    </div>
+                </section>
+                <section>
+                    <div class="container max-width">
+                        
+                    </div>
+                </section>
+            
+            </body>
+            </html>
             `;
         }
     }
-    console.log(data);
 }
-console.log(generateHtml);
+
 
 module.exports = generateHtml;
